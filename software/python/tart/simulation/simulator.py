f20ca59ac571a43110df09764a3969b7e3020bd8
maxscheel <maximilianscheel@gmail.com>
2016-05-03 13:44:21 +1200

    save vis in segments

--- software/python/tart/simulation/simulator.py
+++ software/python/tart/simulation/simulator.py
@@ -26,7 +26,7 @@ def get_vis_parallel(sky, cor, rad, ants, ant_models, config, time, mode='simp')
     p.terminate()
   return ret_vis_list
 
-def get_vis(sky, cor, rad, ants, ant_models, config, timestamp, mode='simp',seed=None):
+def get_vis(sky, cor, rad, ants, ant_models, config, timestamp, mode='simp', seed=None):
   np.random.seed(seed=seed)
   sources = sky.gen_photons_per_src( timestamp, radio=rad, config=config, n_samp=1)
   # sources = sky.gen_n_photons(config, timestamp, radio=rad, n=10)
@@ -44,3 +44,25 @@ def get_vis(sky, cor, rad, ants, ant_models, config, timestamp, mode='simp',seed
     obs = rad.get_simplified_obs(ant_sigs_simp, timestamp, config = config, seed=seed)
     v = cor.correlate(obs)
   return v
+
+
+def get_vis_parallel_segmented(sky, cor, rad, ants, ant_models, config, time, mode='simp', segment_dir='temp/'):
+  import cPickle as pickle
+  vis_per_segment = 1000
+  num_segments = int(np.ceil(len(sky)/float(vis_per_segment)))
+
+  for i in range(num_segments):
+    print 'computing segment %i of %i' %(i, num_segments)
+    start_idx = i*vis_per_segment
+    end_idx = (i+1)*vis_per_segment
+    vis_seg = get_vis_parallel(sky[start_idx:end_idx], cor, rad, ants, ant_models, config, time, mode='simp')
+    pickle.dump( vis_seg, open( segment_dir+'segment_%010i.pkl' %i, "wb" ) )
+
+  #//print 'now combining segments.'
+  ret = []
+  #print num_segments
+  #for i in range(num_segments):
+    #print i
+    #vis_seg = pickle.load( open( segment_dir+'segment_%010i.pkl' %i, "rb" ) )
+    #ret += vis_seg
+  return ret
