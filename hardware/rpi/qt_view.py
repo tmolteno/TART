bf4e994cd5b6ac9ef2c804cad2ab8933b0e5263e
Max Scheel <maximilianscheel@gmail.com>
2017-06-15 05:10:16 +0000

    gracefully end acquisition and processes after saving single vis file (of length chunk).

--- hardware/rpi/qt_view.py
+++ hardware/rpi/qt_view.py
@@ -11,55 +11,73 @@ from multiprocessing import Queue
 from threading import Thread
 
 from pyqtgraph.widgets.RawImageWidget import RawImageWidget
+import time
 
 class QtPlotter(object):
     def __init__(self, app):
         super(QtPlotter, self).__init__()
         self.app = app
         self.create_QtPlotter()
+        self.data = None
 
     def create_QtPlotter(self):
         self.RAW = 1
         if self.RAW:
           self.win = QtGui.QMainWindow()
-          self.rawImg = RawImageWidget(self.win)
+          self.win.resize(400,400)
+          self.rawImg = RawImageWidget(self.win, scaled=True)
           self.win.setCentralWidget(self.rawImg)
-          self.win.resize(128,128)
           self.win.show()
         else:
           self.win = pg.GraphicsLayoutWidget()
           self.win.show()
           self.win.setWindowTitle('TART2 - Live View')
           view = self.win.addViewBox()
-          self.img = pg.ImageItem()
+          view.setAspectLocked(True)
+          self.img = pg.ImageItem(border='w')
           view.addItem(self.img)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
         self.timer.start(0)
         self.q = Queue()
-    
+
+
     def getPort(self):
         return self.q
 
     def update(self):
-        #try:
-        print 'PlotQ size', self.q.qsize()
-        #    if self.q.qsize()> 5:
-        #        [self.q.get() for _ in range(5)]
-        #        print 'dropping frames'
-        #    else:
-        data = self.q.get()
-        #self.imv.setImage(data,autoRange=True,autoLevels=True)
-        d_max = data.max()
-        #print data[0]
-        if self.RAW:
-          data = data/d_max*255
-          self.rawImg.setImage(data)#/d_max)
-        else:
-          data = np.log(data)#*255
-          data = (data - data.min())/data.max()*255
-          self.img.setImage(data)
-        self.app.processEvents()
+        #qsize = self.q.qsize()
+        #print 'PlotQ size',qsize
+        #if qsize > 2:
+            #[self.q.get() for _ in range(qsize-1)]
+            #print '!!!!!!!!!!!!!!!!!!!!!! dropping frames when displaying  !!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+        a = time.time()
+        if not self.q.empty():
+          #while not self.q.empty():
+            #print self.q.qsize()
+            #if self.data is None:
+          self.data = self.q.get()
+            #else:
+              #self.data[:] = self.q.get()
+              #print 'drop drop drop.'
+          #print data[0]
+          b = time.time()
+          print b-a, 'getting data off queue'
+          if self.RAW:
+            d_max = self.data.max()
+            self.data /= d_max
+            self.data *= 255
+            self.rawImg.setImage(self.data)
+          else:
+            self.data -= self.data.min()
+            self.data /= self.data.max()
+            self.data *= 255
+            #self.img.setImage(data,autoRange=True,autoLevels=True)
+            self.img.setImage(self.data)
+          self.app.processEvents()
+          c = time.time()
+          print c-b, 'update done.'
+
         #except Queue.Empty:
         #    pass
 
@@ -67,7 +85,7 @@ def qtLoop():
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
-        
+
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     app = QtGui.QApplication([])
