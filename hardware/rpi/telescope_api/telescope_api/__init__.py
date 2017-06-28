503bc0d7366e6e33a2710305005577df4fbca738
Max Scheel <maximilianscheel@gmail.com>
2017-06-28 05:50:16 +0000

    tidied up authentication

--- hardware/rpi/telescope_api/telescope_api/__init__.py
+++ hardware/rpi/telescope_api/telescope_api/__init__.py
@@ -1,53 +1,32 @@
-#from tartdsp import TartSPI
-
-#import os,sys,inspect
-#currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-#parentdir = os.path.dirname(currentdir)
-#parentdir2 = os.path.dirname(parentdir)
-#sys.path.insert(0,parentdir2)
-
 from tart_dsp.tartspi import *
 from tart_dsp.highlevel_modes_api import *
-import time
-import json
+
 import threading
 import atexit
 
 from flask import Flask
 from flask_cors import CORS, cross_origin
-from flask_apidoc import ApiDoc
+from flask_jwt import JWT
+from werkzeug.security import safe_str_cmp
 
+# Add authentication to app
 class User(object):
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
 
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-
-# Add authentication to app
-from flask_jwt import JWT
-from werkzeug.security import safe_str_cmp
+Admin = User(1,'admin','password')
 
 def authenticate(username, password):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-        return user
+    if safe_str_cmp(Admin.password.encode('utf-8'), password.encode('utf-8')):
+       return Admin
 
 def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
-
+    return Admin
 
 
 POOL_TIME = 1 #Seconds
-
-# Globals:
-users = [User(1, 'admin' , 'password'),User(2,'admin','1')]
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
 
 # variables that are accessible from anywhere
 runtime_config = {}
@@ -132,8 +111,4 @@ def get_config():
 app = create_app()
 
 import telescope_api.views
-
-# add routes to app.
-
-
 
