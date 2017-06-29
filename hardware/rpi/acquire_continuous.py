dc36a9efa597f2198c8cfab9365b564d9445e1ef
Max Scheel <maximilianscheel@gmail.com>
2017-06-29 04:37:57 +0000

    updated CLI tools to latest standard

--- hardware/rpi/acquire_continuous.py
+++ hardware/rpi/acquire_continuous.py
@@ -1,11 +1,7 @@
 import argparse
-#import numpy as np
-#import datetime
-
 import os, stat
 
 import multiprocessing
-#import traceback
 import logging.config
 import yaml
 logger = logging.getLogger(__name__)
@@ -13,12 +9,6 @@ logger = logging.getLogger(__name__)
 from tart_dsp.tartspi import TartSPI
 from tart_dsp.highlevel_modes_api import *
 from tart_dsp.stream_vis import *
-
-from tart.operation import settings
-#from tart.imaging import visibility
-#from tart.imaging import calibration
-#from tart.util import angle
-
 
 if __name__=="__main__":
     PARSER = argparse.ArgumentParser()
@@ -38,74 +28,41 @@ if __name__=="__main__":
             log_config = yaml.load(f.read())
         logging.config.dictConfig(log_config)
 
-
-    # Import result_loop
-
-
-    #from monitor_vis import gen_calib_image
-
-
     runtime_config = {}
-    #runtime_config['mode'] = ARGS.mode
-    runtime_config['mode'] = 'qt'
+    runtime_config['mode'] = ARGS.mode
     runtime_config['blocksize'] = ARGS.blocksize
     runtime_config['telescope_config_path'] = ARGS.config
     runtime_config['calibration_dir'] = ARGS.calibration_dir
-    runtime_config['chunksize'] = 2
-
+    runtime_config['chunksize'] = ARGS.chunksize
+    runtime_config['vis_prefix'] =ARGS.vis_prefix
     tart_instance = TartSPI()
 
-#    post_process = multiprocessing.Process(target=vis_to_latest_image, args=(vis_queue,))
- #   post_process.start()
+    if ARGS.mode in ['save', 'save_single']:
+        single = False
+        if ARGS.mode == 'save_single':
+            single=True
+        vis_queue, vis_calc_process, capture_process, vis_calc_cmd_q, capture_cmd_q = stream_vis_to_queue(tart_instance, runtime_config)
+        from save_vis import result_loop
+        post_process = multiprocessing.Process(target=result_loop, args=(vis_queue, runtime_config,logger,single))
+        post_process.start()
+        post_process.join()
+        capture_cmd_q.put('stop')
+        vis_calc_cmd_q.put('stop')
+        vis_calc_process.join()
+        capture_process.join()
 
-    #vis_calc_process.start()
-    #capture_process.start()
-
-
-
-    #if ARGS.mode == 'save':
-        #vis_calc_process = multiprocessing.Process(target=process_loop, args=(proc_queue, result_queue, settings.Settings(ARGS.config), ARGS.blocksize, None))
-        #from save_vis import result_loop
-        #post_process = multiprocessing.Process(target=result_loop, args=(result_queue, ARGS.chunksize,ARGS.vis_prefix,logger))
-        #capture_process = multiprocessing.Process(target=capture_loop, args=(tart_instance, proc_queue, ARGS.blocksize))
-
-
-    #elif ARGS.mode == 'save_single':
-        #vis_calc_process = multiprocessing.Process(target=process_loop, args=(proc_queue, result_queue, settings.Settings(ARGS.config), ARGS.blocksize, ARGS.chunksize))
-        #from save_vis import result_loop
-        #post_process = multiprocessing.Process(target=result_loop, args=(result_queue, ARGS.chunksize,ARGS.vis_prefix,logger,True))
-        #capture_process = multiprocessing.Process(target=capture_loop, args=(tart_instance, proc_queue, ARGS.blocksize, ARGS.chunksize))
-
-    if ARGS.mode == 'rt_syn_img':
+    elif ARGS.mode == 'rt_syn_img':
         vis_to_latest_image(tart_instance, runtime_config)
 
     elif ARGS.mode == 'qt':
-        vis_queue, vis_calc_process, capture_process vis_calc_cmd_q, capture_cmd_q = stream_vis_to_queue(tart_instance, runtime_config)
-        from pyqtgraph.Qt import QtGui, QtCore
-        import qt_view
-        print 'importing alternative result_loop'
+        vis_queue, vis_calc_process, capture_process, vis_calc_cmd_q, capture_cmd_q = stream_vis_to_queue(tart_instance, runtime_config)
         from monitor_vis import result_loop
 
+        from pyqtgraph.Qt import QtGui, QtCore
+        import qt_view
         app = QtGui.QApplication([])
         plotter = qt_view.QtPlotter(app)
         plotQ = plotter.getPort()
         post_process = multiprocessing.Process(target=result_loop, args=(vis_queue, runtime_config['chunksize'], runtime_config['mode'], plotQ, runtime_config['calibration_dir']))
         post_process.start()
         QtGui.QApplication.instance().exec_()
-
-
-    # Start processes in reverse order of chain
-    #vis_calc_process.start()
-    #capture_process.start()
-    #print 'Process chain started.'
-
-    #if ARGS.mode == 'save_single':
-        #post_process.join()
-        #vis_calc_process.join()
-        #capture_process.join()
-        #print 'Process chain finished.'
-
-
-    #if ARGS.mode == 'qt':
-        #QtGui.QApplication.instance().exec_()
-
--- hardware/rpi/save_vis.py
+++ hardware/rpi/save_vis.py
@@ -2,7 +2,7 @@ import time, os
 import traceback
 from tart.imaging import visibility
 
-def result_loop(result_queue, chunk_size, vis_prefix, logger, single=False):
+def result_loop(result_queue, runtime_config, logger, single=False):
     vislist = []
     active = True
     while(active):
@@ -10,19 +10,16 @@ def result_loop(result_queue, chunk_size, vis_prefix, logger, single=False):
             try:
                 vis, means = result_queue.get()
                 vislist.append(vis)
-                if len(vislist)>=chunk_size:
-                    fname = vis_prefix + "_" + vis.timestamp.strftime('%Y-%m-%d_%H_%M_%S.%f')+".vis"
+                if len(vislist)>=runtime_config['chunksize']:
+                    fname = runtime_config['vis_prefix'] + "_" + vis.timestamp.strftime('%Y-%m-%d_%H_%M_%S.%f')+".vis"
                     print 'saved ', vis, ' to', fname
                     visibility.Visibility_Save(vislist, fname)
                     if single:
                       active = False
-                    #import signal, os
-                    #os.kill(os.getpid(), signal.SIGUSR1)
-
                     vislist = []
             except Exception, e:
                 logger.error( "PostProcessing Error %s" % str(e))
                 logger.error(traceback.format_exc())
         else:
-            time.sleep(0.00001)
+            time.sleep(0.001)
     return 1
