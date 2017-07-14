0eb7cf955f985359d36dd76cbd183779d8035bba
milletf1 <timmiller83@gmail.com>
2017-07-14 17:07:14 +1200

    Drawing antenna positions from static antenna position data in diagnose mode.

--- web-app/src/app/models/ChannelStatus.ts
+++ web-app/src/app/models/ChannelStatus.ts
@@ -3,9 +3,9 @@
  * @author Tim Miller
  */
 export class ChannelStatus {
-    id: number;
-    phase: any; // { N_samples: number, measured: number, ok: number, stability: number, threshold: number }
-    radioMean: any; // { mean: number, ok: number, threshold: number }
+    public id: number;
+    public phase: any; // { N_samples: number, measured: number, ok: number, stability: number, threshold: number }
+    public radioMean: any; // { mean: number, ok: number, threshold: number }
 
     constructor(id: number, phase: any, radioMean: any) {
         this.id = id;
