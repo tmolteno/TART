076e4daeaee857963cb4a92d59681b24b90ac0b8
milletf1 <timmiller83@gmail.com>
2017-07-19 14:30:55 +1200

    Created mock auth service for use in testing.

---
+++ web-app/src/app/testing/mock.auth.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+
+/**
+ * Mock auth service.
+ * This should be provided when testing components that rely on auth service.
+ */
+@Injectable()
+export class MockAuthService {
+
+    private authToken: string;
+    private loginObserver: any;
+    private loginStatus: boolean;
+    public login$: Observable<boolean>;
+
+    constructor() {
+        this.authToken = 'fake_token';
+        this.loginStatus = strue;
+        this.login$ = new Observable(observer => {
+            this.loginObserver = observer;
+        }).share();
+    }
+
+    login(password: string) {
+        return Observable.create(observer => {
+            console.log("in observable function...");
+            let result = false;
+
+            if (password === 'password') {
+                this.setAuthToken('fake_token');
+                result = true;
+            }
+            observer.next(result);
+            observer.complete();
+        });
+    }
+
+    load() {
+        this.loginObserver.next();
+    }
+
+    logout() {
+        this.authToken = null;
+    }
+
+    setAuthToken(token: string) {
+        this.authToken = token;
+    }
+
+    isTokenValid() {
+        return this.authToken !== null;
+    }
+
+    getAuthToken() {
+        if (!this.isTokenValid()) {
+            return null;
+        }
+        return this.authToken;
+    }
+
+    getAuthRequestOptions() {
+        if (!this.isTokenValid()) {
+            throw new Error('token not found or expired');
+        }
+        return new RequestOptions({
+            headers: new Headers({
+                'Authorization': `JWT ${this.authToken}`
+            })
+        });
+    }
+}
--- web-app/src/app/views/cal-mode/cal-mode.component.spec.ts
+++ web-app/src/app/views/cal-mode/cal-mode.component.spec.ts
@@ -1,17 +1,39 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { DebugElement } from '@angular/core';
+import {
+     HttpModule,
+     Http,
+     Response,
+     ResponseOptions,
+     XHRBackend
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { CalModeComponent } from './cal-mode.component';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../testing/mock.auth.service';
+import { ModeService } from '../../services/mode.service';
 
 describe('CalModeComponent', () => {
   let component: CalModeComponent;
   let fixture: ComponentFixture<CalModeComponent>;
+  let authService: AuthService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalModeComponent ]
+      imports: [HttpModule],
+      providers: [
+          { provide: XHRBackend, useClass: MockBackend },
+          { provide: AuthService, useClass: MockAuthService },
+          { provide: Router, useClass: class {
+              navigate = jasmine.createSpy('navigate');
+          }},
+          ModeService
+      ],
+      declarations: [CalModeComponent]
     })
     .compileComponents();
   }));
@@ -23,6 +45,6 @@ describe('CalModeComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(component).toBeTruthy();
   });
 });
