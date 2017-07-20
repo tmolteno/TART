1a1bf96418168029988e896654223289e7c98be9
Tim Molteno <tim@elec.ac.nz>
2017-07-20 13:19:07 +1200

    colourmap as [optional] argument for gen_image

--- web-app/api_imaging/example/app.js
+++ web-app/api_imaging/example/app.js
@@ -9,19 +9,6 @@ var sat = fake_data.sat;
 var nw = 60; // Number of wavelengths
 var num_bin = Math.pow(2,9); // bins in the fft
 
-const scale = require('scale-color-perceptual')
-const hex2rgb = require('scale-color-perceptual/utils/hex2rgb')
-
-
-function apply_colormap(imagedata){
-  var end = imagedata.data.length;
-  for (var i=0; i<end; i+=4) {
-    val = hex2rgb(scale.viridis(imagedata.data[i]/255.))
-    imagedata.data[i]   = val.r*255;
-    imagedata.data[i+1] = val.g*255;
-    imagedata.data[i+2] = val.b*255;
-  }
-}
 
 function get_max_ang(nw, num_bin){
   var max_ang = (num_bin/(4*nw) * 180/Math.PI);
@@ -93,11 +80,10 @@ function overlay_satellites(ctx, sat_list, nw, num_bin, ){
 }
 
 window.onload = function() {
-  var dummyCanvas = api_synthesis.gen_image(vis, ant_pos, calib, nw, num_bin);
+  var dummyCanvas = api_synthesis.gen_image(vis, ant_pos, calib, nw, num_bin,'electric');
   var dummyImageData = dummyCanvas.getContext('2d').getImageData(0,0,dummyCanvas.width,dummyCanvas.height);
   var myCanvas = document.getElementById('my-canvas');
   var ctx = myCanvas.getContext('2d');
-  apply_colormap(dummyImageData);
   ctx.putImageData(dummyImageData, 0, 0);
   overlay_grid(ctx,nw, num_bin);
   overlay_satellites(ctx,sat, nw, num_bin);
