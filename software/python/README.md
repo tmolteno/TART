e0ec6fcd5dee83e3235bc362ddbeb888f1c8956f
Tim Molteno <tim@bling.elec.ac.nz>
2017-08-31 15:02:15 +1200

    Added neccesary files to use the python packaging index

--- software/python/README.md
+++ software/python/README.md
@@ -1,12 +1,14 @@
-#TART: operating software
-
-##tart 
-    Python package software. This should be installed using 
-    python setyp.py install
+# TART: operating software
     
-##tools
+## tools
     Software for operation and analysis of data
 
+
 ## Software Dependencies
+
     aptitude install python-numpy python-matplotlib python-setuptools pkg-config python-scipy python-psycopg2 libfftw3-dev
     easy_install jsonrpclib pyfits healpy pyfftw json
+
+## Development work
+    Python package software. This should be installed using 
+    python setyp.py install
--- software/python/setup.py
+++ software/python/setup.py
@@ -2,14 +2,25 @@
 # from distutils.core import setup
 from setuptools import setup, find_packages
 
+with open('README.txt') as f:
+    readme = f.read()
+
 setup(name='tart',
-      version='0.14',
-      description='Transient Array Radio Telescope Imaging and Operation Library',
-      url='http://github.com/tmolteno/projects/TART',
-      author='Tim Molteno',
-      author_email='tim@elec.ac.nz',
-      requires=['numpy'],
-      license='GPLv3',
-      packages=['tart', 'tart.imaging', 'tart.simulation', 'tart.operation', 'tart.util',\
-       'tart.test',  'tart.imaging.test', 'tart.simulation.test', 'tart.operation.test', 'tart.util.test'],
-)
+    version='0.14.1',
+    description='Transient Array Radio Telescope Imaging and Operation Library',
+    long_description=readme,
+    url='http://github.com/tmolteno/projects/TART',
+    author='Tim Molteno',
+    author_email='tim@elec.ac.nz',
+    license='GPLv3',
+    requires=['numpy'],
+    packages=['tart', 'tart.imaging', 'tart.simulation', 'tart.operation', 'tart.util',\
+            'tart.test',  'tart.imaging.test', 'tart.simulation.test', 'tart.operation.test', 'tart.util.test'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Communications :: Ham Radio",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        "Intended Audience :: Science/Research"])
