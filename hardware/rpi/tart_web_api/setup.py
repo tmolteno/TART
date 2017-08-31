6967e92fa439b56d0900e66b6456101e9dba4017
tmolteno <tim@molteno.net>
2017-08-31 16:33:11 +1200

    Added dependency on tart

--- hardware/rpi/tart_web_api/setup.py
+++ hardware/rpi/tart_web_api/setup.py
@@ -8,7 +8,7 @@ setup(
     name='tart_web_api',
     description='Transient Array Radio Telescope High-level HTTP Interface',
     long_description=readme,
-    version='0.1',
+    version='0.1.1',
     packages=['tart_web_api'],
     include_package_data=True,
     install_requires=[
@@ -18,7 +18,8 @@ setup(
         'flask-cli',
         'flask-cors',
         'flask-script',
-        'tart_hardware_interface'
+        'tart_hardware_interface',
+        'tart'
     ],
     url='http://github.com/tmolteno/projects/TART',
     author='Tim Molteno, Max Scheel, Pat Suggate',
