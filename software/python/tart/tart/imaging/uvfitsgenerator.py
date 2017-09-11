d81bc07ff86dde65e4d0462cd3b78dc4c5f449e6
Tim Molteno <tim@elec.ac.nz>
2017-09-11 21:56:43 +1200

    Fixed up bitrot.

--- software/python/tart/tart/imaging/uvfitsgenerator.py
+++ software/python/tart/tart/imaging/uvfitsgenerator.py
@@ -11,6 +11,8 @@ except:
 import numpy as np
 
 from tart.imaging import tart_util
+from tart.imaging import radio_source
+from tart.imaging import location
 
 from tart.simulation import antennas
 
@@ -47,35 +49,49 @@ def decode_baseline(bl):
 
 class UVFitsGenerator(object):
   ''' Generate a UVFITS file for a target source from an array of visibilities.'''
-  def __init__(self, cal_vis, phase_center): # FIXME
+  def __init__(self, cal_vis_list, phase_center): # FIXME
     # self.phase_center_elevation = phase_center_elevation
     # self.phase_center_azimuth = phase_center_azimuth
+    vt = cal_vis_list[0]
 
+    c = vt.get_config()
+    ant_p = np.asarray(c.get_antenna_positions())
+
+    bls = np.asarray(vt.get_baselines())
+
+    self.config = c
     self.v_array = cal_vis_list
-    self.n_baselines = len(self.v_array[0].baselines)
-    self.loc = self.v_array[0].get_config().get_loc() # Assume the array isn't moving
-    self.phase_center = phase_center
+    self.n_baselines = len(bls)
+    self.loc = c.get_loc() # Assume the array isn't moving
+    
+    if phase_center is None:
+        ra, dec = location.get_loc(vt.get_config()).horizontal_to_equatorial(vt.get_timestamp(), angle.from_dms(90), angle.from_dms(0))
+        self.phase_center = radio_source.CosmicSource(ra, dec, 1e10)
+    else:
+        self.phase_center = phase_center
 
   def gen_vis_table(self):
     '''Generate visibility table and fill payload.'''
 
     baselines = []
+    ant_pos = self.config.get_antenna_positions()
+    loc = self.config.get_loc()
     for v in self.v_array:
-      ra, dec = self.phase_center.radec(v.timestamp)
-      v.rotate(skyloc.Skyloc(ra, dec))
-      for i in range(v.config.num_baselines):
-        c = v.get_config()
-        a0 = antennas.Antenna(c.get_loc(), c.ant_positions[v.baselines[i][0]])
-        a1 = antennas.Antenna(c.get_loc(), c.ant_positions[v.baselines[i][1]])
+      ra, dec = self.phase_center.radec(v.vis.timestamp)
+      #v.rotate(skyloc.Skyloc(ra, dec))
+      for i in range(self.n_baselines):
+        c = self.config
+        a0 = antennas.Antenna(loc, ant_pos[v.vis.baselines[i][0]])
+        a1 = antennas.Antenna(loc, ant_pos[v.vis.baselines[i][1]])
         baseline = {}
-        uu, vv, ww = antennas.get_UVW(a0, a1, v.timestamp, ra, dec)
+        uu, vv, ww = antennas.get_UVW(a0, a1, v.vis.timestamp, ra, dec)
         # print (np.array(a1.enu) - np.array(a0.enu)), uu, vv, ww
         # arcane units of UVFITS require u,v,w in nanoseconds
         baseline['UU'] = uu/constants.V_LIGHT
         baseline['VV'] = vv/constants.V_LIGHT
         baseline['WW'] = ww/constants.V_LIGHT
-        baseline['BASELINE'] = encode_baseline(v.baselines[i][0]+1, v.baselines[i][1]+1)
-        baseline['DATE'] = tart_util.get_julian_date(v.timestamp)- int(tart_util.get_julian_date(v.timestamp)+0.5)
+        baseline['BASELINE'] = encode_baseline(v.vis.baselines[i][0]+1, v.vis.baselines[i][1]+1)
+        baseline['DATE'] = tart_util.get_julian_date(v.vis.timestamp)- int(tart_util.get_julian_date(v.vis.timestamp)+0.5)
         # DATE FIXME ?
         baselines.append(baseline)
 
@@ -85,11 +101,11 @@ class UVFitsGenerator(object):
     data = np.zeros((len(self.v_array)*self.n_baselines, 1, 1, len(freqs), len(pols), 3))
 
     for i, v in enumerate(self.v_array):
-      for k,  b in enumerate(v.baselines):
+      for k,  b in enumerate(v.vis.baselines):
         for l, _ in enumerate(freqs):
           for j, _ in enumerate(pols):
-            re = v.v[k].real
-            img = v.v[k].imag
+            re = v.vis.v[k].real
+            img = v.vis.v[k].imag
             w = np.ones(1)
             data[i*self.n_baselines+k, 0, 0, l, j, :] = [re, img, w]
 
@@ -105,12 +121,13 @@ class UVFitsGenerator(object):
   def gen_ant_table(self):
     '''Generate antenna table.'''
     v = self.v_array[0]
-    n_ant = v.config.num_antennas
+    n_ant = self.config.get_num_antenna()
+    ant_pos = self.config.get_antenna_positions()
     v.antennas = []
     for i in range(n_ant):
       antenna = {}
       antenna['ANNAME']  = 'ANT%i'% (i+1)     # there is no antenna 0
-      antenna['STABXYZ'] = v.config.ant_positions[i]
+      antenna['STABXYZ'] = ant_pos[i]
       antenna['ORBPARM'] = 0. # SET TO 0. - not used, because 'NUMORB' in header is  0
       antenna['NOSTA']   = i+1                # there is no antenna 0
       antenna['MNTSTA']  = 0
@@ -146,40 +163,40 @@ class UVFitsGenerator(object):
     params = ["UU", "VV", "WW", "BASELINE", "DATE"]
     ctypes = ["STOKES", "FREQ", "RA", "DEC"]
 
-    self.vis_header.update('OBJECT', 'Full Sky')
+    self.vis_header.set('OBJECT', 'Full Sky')
 
     # without rotation of the visibilities is has to be this:
-    # ra, dec = self.loc.horizontal_to_equatorial(v.timestamp, angle.from_dms(90.), angle.from_dms(0.))
+    # ra, dec = self.loc.horizontal_to_equatorial(v.vis.timestamp, angle.from_dms(90.), angle.from_dms(0.))
     # with rotation is has to be:
 
-    ra, dec = self.phase_center.radec(v.timestamp)
+    ra, dec = self.phase_center.radec(v.vis.timestamp)
 
-    self.vis_header.update('OBSRA', ra.to_degrees())	# Antenna pointing center
-    self.vis_header.update('OBSDEC', dec.to_degrees())	# Antenna pointing center
+    self.vis_header.set('OBSRA', ra.to_degrees())	# Antenna pointing center
+    self.vis_header.set('OBSDEC', dec.to_degrees())	# Antenna pointing center
 
-    self.vis_header.update('TELESCOP', 'TART')
-    self.vis_header.update('INSTRUME', 'TART')
-    self.vis_header.update('EPOCH', 2000.0)
-    self.vis_header.update('BSCALE', 1)
-    self.vis_header.update('DATE-OBS', '%i-%02i-%02iT00:00:00.0'%(v.timestamp.year, v.timestamp.month, v.timestamp.day))
+    self.vis_header.set('TELESCOP', 'TART')
+    self.vis_header.set('INSTRUME', 'TART')
+    self.vis_header.set('EPOCH', 2000.0)
+    self.vis_header.set('BSCALE', 1)
+    self.vis_header.set('DATE-OBS', '%i-%02i-%02iT00:00:00.0'%(v.vis.timestamp.year, v.vis.timestamp.month, v.vis.timestamp.day))
 
     for i, par in enumerate(params):
-      self.vis_header.update('PTYPE%i' % (i+1), par)
-      self.vis_header.update('PSCAL%i' % (i+1), 1.0)
-      self.vis_header.update('PZERO%i' % (i+1), 0.0)
+      self.vis_header.set('PTYPE%i' % (i+1), par)
+      self.vis_header.set('PSCAL%i' % (i+1), 1.0)
+      self.vis_header.set('PZERO%i' % (i+1), 0.0)
 
-    self.vis_header.update('PZERO5', int(tart_util.get_julian_date(v.timestamp) + 0.5))
+    self.vis_header.set('PZERO5', int(tart_util.get_julian_date(v.vis.timestamp) + 0.5))
 
-    self.vis_header.update('CRVAL2', 1.0)
-    self.vis_header.update('CRPIX2', 1.0)
-    self.vis_header.update('CDELT2', 1.0)
-    self.vis_header.update('CTYPE2', 'COMPLEX')
+    self.vis_header.set('CRVAL2', 1.0)
+    self.vis_header.set('CRPIX2', 1.0)
+    self.vis_header.set('CDELT2', 1.0)
+    self.vis_header.set('CTYPE2', 'COMPLEX')
 
     for i, ctyp in enumerate(ctypes):
-      self.vis_header.update('CTYPE%d' % (i+3), ctyp)
-      self.vis_header.update('CRVAL%d' % (i+3), 1.0)
-      self.vis_header.update('CRPIX%d' % (i+3), 1.0)
-      self.vis_header.update('CDELT%d' % (i+3), 1.0)
+      self.vis_header.set('CTYPE%d' % (i+3), ctyp)
+      self.vis_header.set('CRVAL%d' % (i+3), 1.0)
+      self.vis_header.set('CRPIX%d' % (i+3), 1.0)
+      self.vis_header.set('CDELT%d' % (i+3), 1.0)
 
     #set the input pol type.
     #for circular CRVAL3 should be -1, CDELT3 -1
@@ -190,20 +207,20 @@ class UVFitsGenerator(object):
 
     pol_typ = 1.  # FIXME
     #pol_typ =  Obs.pol_typ
-    self.vis_header.update('CRPIX3', pol_typ)
+    self.vis_header.set('CRPIX3', pol_typ)
     if pol_typ < 0:
-      self.vis_header.update('CDELT3', -1.)
+      self.vis_header.set('CDELT3', -1.)
     else:
-      self.vis_header.update('CDELT3', 1.)
+      self.vis_header.set('CDELT3', 1.)
 
-    self.vis_header.update('CRPIX4', 1.)  # something .... n_freq/2 +1
-    self.vis_header.update('CDELT4', v.config.bandwidth) # freq delta
-    self.vis_header.update('CRVAL4', v.config.frequency) # center freq
+    self.vis_header.set('CRPIX4', 1.)  # something .... n_freq/2 +1
+    self.vis_header.set('CDELT4', v.vis.config.get_bandwidth()) # freq delta
+    self.vis_header.set('CRVAL4', v.vis.config.get_operating_frequency()) # center freq
 
-    ra, dec = self.phase_center.radec(v.timestamp)
+    ra, dec = self.phase_center.radec(v.vis.timestamp)
 
-    self.vis_header.update('CRVAL5', ra.to_degrees())	  #SOURCE RA  PARAMETER
-    self.vis_header.update('CRVAL6', dec.to_degrees())	#SOURCE DEC PARAMETER
+    self.vis_header.set('CRVAL5', ra.to_degrees())	  #SOURCE RA  PARAMETER
+    self.vis_header.set('CRVAL6', dec.to_degrees())	#SOURCE DEC PARAMETER
 
     self.vis_header.add_history("AIPS WTSCAL =  1.0")  # has to be in here for some reason
     self.vis_header.add_comment("written by the TART UV FITS writer. ELEC Otago Uni")
@@ -213,40 +230,40 @@ class UVFitsGenerator(object):
 
     v = self.v_array[0]
 
-    self.ant_header.update('TFORM3', '1J')  #
-    self.ant_header.update('TFORM4', '1J')    #
-    self.ant_header.update('TFORM5', '1E')     #   replace (J,E,D) with (1J,1E,1D) to match difmap requirements
-    self.ant_header.update('TFORM7', '1E')     # # ra and dec of sun here! FIXME - move to fixed phase center in ra/dec
-    self.ant_header.update('TFORM10', '1E')   #
-    self.ant_header.update('TFORM12', '1D') #
+    self.ant_header.set('TFORM3', '1J')  #
+    self.ant_header.set('TFORM4', '1J')    #
+    self.ant_header.set('TFORM5', '1E')     #   replace (J,E,D) with (1J,1E,1D) to match difmap requirements
+    self.ant_header.set('TFORM7', '1E')     # # ra and dec of sun here! FIXME - move to fixed phase center in ra/dec
+    self.ant_header.set('TFORM10', '1E')   #
+    self.ant_header.set('TFORM12', '1D') #
 
-    self.ant_header.update('EXTNAME','AIPS AN')
-    self.ant_header.update('XTENSION', 'BINTABLE')
+    self.ant_header.set('EXTNAME','AIPS AN')
+    self.ant_header.set('XTENSION', 'BINTABLE')
     arr_loc = self.loc.get_ecef()
-    self.ant_header.update('ARRAYX', arr_loc[0]) #
-    self.ant_header.update('ARRAYY', arr_loc[1])  #  TART POSITION in ECEF coordinates
-    self.ant_header.update('ARRAYZ', arr_loc[2]) #
-    self.ant_header.update('FREQ', v.config.frequency)
+    self.ant_header.set('ARRAYX', arr_loc[0]) #
+    self.ant_header.set('ARRAYY', arr_loc[1])  #  TART POSITION in ECEF coordinates
+    self.ant_header.set('ARRAYZ', arr_loc[2]) #
+    self.ant_header.set('FREQ', v.vis.config.get_operating_frequency())
     #/* GSTIAO is the GST at zero hours in the time system of TIMSYS (i.e. UTC) */
     #mjd = trunc(data->date[0] - 2400000.5);
     #temp = slaGmst(mjd)*180.0/M_PI;  # technically, slaGmst takes UT1, but it won't matter here.
     #fits_update_key(fptr,TDOUBLE,"GSTIA0",&temp , NULL, &status);
-    self.ant_header.update('GSTIA0', self.loc.GST(v.timestamp).to_degrees())
-    self.ant_header.update('DEGPDY', 3.60985642988E+02) # Rotation per day
+    self.ant_header.set('GSTIA0', self.loc.GST(v.vis.timestamp).to_degrees())
+    self.ant_header.set('DEGPDY', 3.60985642988E+02) # Rotation per day
 
-    self.ant_header.update('RDATE', "%d-%02d-%02dT00:00:00.0"%(v.timestamp.year, v.timestamp.month, v.timestamp.day))
-    self.ant_header.update('POLARX', 0.0)
-    self.ant_header.update('POLARY', 0.0)
-    self.ant_header.update('UT1UTC', 0.0)
-    self.ant_header.update('DATUTC', 0.0)
-    self.ant_header.update('TIMSYS', 'UTC')
-    self.ant_header.update('ARRNAM', 'TART')
-    self.ant_header.update('NUMORB', 0)
-    self.ant_header.update('NOPCAL', 3)
-    self.ant_header.update('FREQID', -1)
-    self.ant_header.update('IATUTC', 33.0)
-    self.ant_header.update('EXTVER', 1)
-    #self.ant_header.update('XYZHAND','RIGHT')
+    self.ant_header.set('RDATE', "%d-%02d-%02dT00:00:00.0"%(v.vis.timestamp.year, v.vis.timestamp.month, v.vis.timestamp.day))
+    self.ant_header.set('POLARX', 0.0)
+    self.ant_header.set('POLARY', 0.0)
+    self.ant_header.set('UT1UTC', 0.0)
+    self.ant_header.set('DATUTC', 0.0)
+    self.ant_header.set('TIMSYS', 'UTC')
+    self.ant_header.set('ARRNAM', 'TART')
+    self.ant_header.set('NUMORB', 0)
+    self.ant_header.set('NOPCAL', 3)
+    self.ant_header.set('FREQID', -1)
+    self.ant_header.set('IATUTC', 33.0)
+    self.ant_header.set('EXTVER', 1)
+    #self.ant_header.set('XYZHAND','RIGHT')
 
   def write(self, filename):
     '''Generate tables and headers and write UVFITS file.'''
