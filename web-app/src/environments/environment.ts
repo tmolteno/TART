f90ffb6f34e1a4da17a667a8e13979063abe7672
Tim Molteno <tim@physics.otago.ac.nz>
2017-09-27 12:39:47 +1300

    Update proxy to use new upstream server

--- web-app/src/environments/environment.ts
+++ web-app/src/environments/environment.ts
@@ -2,8 +2,12 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
+// 
+// For Internal Development use the following.
+//   apiUrl: 'http://tart2-orange/'
 
 export const environment = {
   production: false,
-  apiUrl: 'http://tart2-raspberry'
+  //apiUrl: "https://tart.elec.ac.nz/signal/"
+  apiUrl: 'http://tart2-pear/'
 };
