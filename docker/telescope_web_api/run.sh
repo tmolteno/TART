d4baeeb11e0feb4b84e678816929c9a2ae0be5cc
tmolteno <tim@molteno.net>
2018-08-30 22:30:59 +1200

    Added a volume to mount

--- docker/telescope_web_api/run.sh
+++ docker/telescope_web_api/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Run the docker image. The login password must be specified here.
 #
-docker run -e LOGIN_PW=password -p 5000:5000 --name=web_api -it  tart_web_api
+# Persistent storage is in the folder ~/web_api_store
+#
+docker run -e LOGIN_PW=password -p 5000:5000 \
+    -v ~/web_api_store:/app \
+    --name=web_api -it  tart_web_api
 # twistd -n web --port 8080 --wsgi tart_web_api.app
