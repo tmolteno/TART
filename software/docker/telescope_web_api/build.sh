cb160a01a8aa893deb222009f3fe7a60fba5ff4d
tim <tim@elec.ac.nz>
2018-09-14 10:42:29 +1200

    Move to slim images and build the web api documentation as part of the container build process. Then make this available from the telescope web front end in /doc

--- software/docker/telescope_web_api/build.sh
+++ software/docker/telescope_web_api/build.sh
@@ -1,4 +1,1 @@
-rm -rf app
-mkdir -p app
-cp -a ../../hardware/rpi/24_ant_setup app/
 docker build -t tart_web_api .
--- software/docker/web_app/build.sh
+++ software/docker/web_app/build.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-rm -rf app
-cp -a ../../web-app app
 docker build -t tart_web_front_end:prod .
