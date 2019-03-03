786c72f3de5f084c134af57d971a4ed8e6ab7a38
Tim Molteno <tim@elec.ac.nz>
2019-03-04 08:44:22 +1300

    Added a tunelling comment

--- software/containers/autossh/entrypoint.sh
+++ software/containers/autossh/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 chmod 0400 /id_rsa
+echo Tunneling: -R ${SSH_TUNNEL_REMOTE}:${SSH_TUNNEL_HOST}:${SSH_TUNNEL_LOCAL} \
+            ${SSH_HOSTUSER}@${SSH_HOSTNAME}
 
 autossh -M 0 -N -T -C -i /id_rsa \
             -o StrictHostKeyChecking=no \
