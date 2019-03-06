c420e60a710f51715bdb05601f9f934c578c89ac
tim <tim@elec.ac.nz>
2019-03-07 10:37:29 +1300

    Add exception handling

--- hardware/rpi/tart_cli/get_data_cli.py
+++ hardware/rpi/tart_cli/get_data_cli.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
-#    Upload antenna positions from local file to remote telescope
-#    Tim Molteno 2017 - tim@elec.ac.nz
+#    Download Raw Data from a TART telescope
+#    The telescope should be in RAW mode if you want it to capture raw data every minute.
+#
+#    Tim Molteno 2017-2019 - tim@elec.ac.nz
 
 import argparse
 import json
 
-
-import os, stat
-import requests
-from requests.auth import HTTPDigestAuth
+import os
 import hashlib
 import urllib.request, urllib.error, urllib.parse
 import time
 
 from tart_tools.api_handler import AuthorizedAPIhandler
-
-def get_from_api(view, api_endpoint):
-    myResponse = requests.get(api_endpoint+view,)
-    if(myResponse.ok):
-        jData = json.loads(myResponse.content)
-    else:
-        myResponse.raise_for_status()
-    return jData
 
 def sha256_checksum(filename, block_size=65536):
     sha256 = hashlib.sha256()
@@ -34,10 +25,12 @@ def download_file(url, checksum=0):
     print("Download_file({}, {})".format(url, checksum))
     file_name = url.split('/')[-1]
     u = urllib.request.urlopen(url)
-    f = open(file_name, 'wb')
+    
+    file_path = os.path.join(ARGS.dir, file_name)
+    f = open(file_path, 'wb')
     meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print("Downloading: %s Bytes: %s" % (file_name, file_size))
+    file_size = int(meta.get_all("Content-Length")[0])
+    print("Downloading: %s Bytes: %s" % (file_path, file_size))
 
     file_size_dl = 0
     block_sz = 8192
@@ -52,14 +45,14 @@ def download_file(url, checksum=0):
         print(status, end=' ')
     f.close()
     if checksum:
-        if (sha256_checksum(file_name) != checksum):
-            os.remove(file_name)
+        if (sha256_checksum(file_path) != checksum):
+            os.remove(file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get RAW data from the telescope', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--api', required=False, default='https://tart.elec.ac.nz/signal', help="Telescope API server URL.")
     parser.add_argument('--pw', default='password', type=str, help='API password')
-    parser.add_argument('--file', type=str, required=True, help='local file to download')
+    parser.add_argument('--dir', type=str, default=".", help='local directory to download')
 
     ARGS = parser.parse_args()
 
@@ -75,18 +68,22 @@ if __name__ == '__main__':
         resp_raw = api.get('raw/data')
         resp_vis = api.get('vis/data')
 
-        for entry in resp_raw+resp_vis:
-            if 'filename' in entry:
-                data_url = urllib.parse.urljoin(tart_endpoint, entry['filename'])
-                file_name = data_url.split('/')[-1]
-                if os.path.isfile(file_name):
-                    if (sha256_checksum(file_name) == entry['checksum']):
-                        print('Skipping', file_name)
+        try:
+            for entry in resp_raw+resp_vis:
+                if 'filename' in entry:
+                    data_url = urllib.parse.urljoin(tart_endpoint, entry['filename'])
+                    file_name = data_url.split('/')[-1]
+                    if os.path.isfile(file_name):
+                        if (sha256_checksum(file_name) == entry['checksum']):
+                            print('Skipping', file_name)
+                        else:
+                            print('Corrupted File', file_name)
+                            os.remove(file_name)
+                            download_file(data_url, entry['checksum'])
                     else:
-                        print('Corrupted File', file_name)
-                        os.remove(file_name)
                         download_file(data_url, entry['checksum'])
-                else:
-                    download_file(data_url, entry['checksum'])
-        time.sleep(5)
+        except Exception as e:
+            print("Exception "+str(e))
+        finally:
+            time.sleep(2)
 
