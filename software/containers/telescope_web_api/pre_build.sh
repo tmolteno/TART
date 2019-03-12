d48187c9a5c02a67e06acd8b09d85f3edd49340b
tim <tim@elec.ac.nz>
2019-03-12 13:34:18 +1300

    Clean up temporary directories

--- software/containers/telescope_web_api/pre_build.sh
+++ software/containers/telescope_web_api/pre_build.sh
@@ -1,3 +1,5 @@
-cp -a ../../python_modules/tart_web_api .
-cp -a ../../python_modules/tart .
-cp -a ../../python_modules/tart_hardware_interface .
+# Copy python modules into the local code directory
+mkdir -p python_code/
+cp -a ../../python_modules/tart_web_api python_code/
+cp -a ../../python_modules/tart python_code/
+cp -a ../../python_modules/tart_hardware_interface python_code/
