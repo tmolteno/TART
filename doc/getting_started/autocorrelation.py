f1bcb41a1e098f415adc5471e352c693e541cee6
tim <tim@elec.ac.nz>
2022-10-28 09:43:50 +1300

    Update autocorrelation, see https://github.com/tart-telescope/tart2ms/issues/18

--- doc/getting_started/autocorrelation.py
+++ doc/getting_started/autocorrelation.py
@@ -14,6 +14,7 @@
 #   python3 image_from_raw.py --file  ~/Downloads/data_2019-07-24_22_47_14.353231.hdf
 
 import argparse
+import scipy.signal
 
 import numpy as np
 import numpy.fft as fft
@@ -21,10 +22,39 @@ import numpy.fft as fft
 import matplotlib.pyplot as plt
 
 from tart.operation import observation
-from tart.imaging import correlator
-from tart.util import constants
 
-from tart_tools import api_handler
+
+def acorr(y):
+    ret = scipy.signal.correlate(y, y, mode = 'full')
+    return ret
+
+def estimate_weights(obs):
+    '''
+    The variance of visibility v_ij is v_ii * v_jj / K, in other words the product of the
+    corresponding autocorrelations divided by the number of samples that went into
+    calculating the visibility. This is based on Wishart statistics. The weight is therefore
+    K / (v_ii * v_jj). This is accessed in katdal as the weights property on the dataset
+    (and is written to the WEIGHT_SPECTRUM column when converting to MS).
+    '''
+    n_ant = obs.config.get_num_antenna()
+    y = obs.get_antenna(0)
+    n = y.shape[0]
+
+    K = n / 2  # Number of samples
+    
+    variances = [np.sum(acorr(obs.get_antenna(i)))/np.sqrt(n) for i in range(n_ant)]
+    print(variances)
+    weights = np.zeros((n_ant, n_ant))
+    for i in range(n_ant):
+        for j in range(n_ant):
+            v_ii = variances[i]
+            v_jj = variances[j]
+            weights[i,j] = K / (v_ii * v_jj)
+            
+    return weights
+
+    
+
 
 '''
     Create an image from raw (hdf5) data downloaded from the TART telescope
@@ -35,8 +65,7 @@ from tart_tools import api_handler
         tart_set_mode --vis --pw <password>
         
     Step 2: Run this file
-        python3 image_from_raw.py --file ./data_2022-06-21_01_57_06.161084.hdf
-
+        python3 autocorrelation.py --file ./data_2022-06-21_01_57_06.161084.hdf
 '''
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser(description='Create an image from raw (hdf5) data downloaded from the TART telescope.')
@@ -52,11 +81,23 @@ if __name__ == '__main__':
     # Load the Observation file
     obs = observation.Observation_Load(ARGS.file)
 
+    w = estimate_weights(obs)
+    print(w)
     n_ant = obs.config.get_num_antenna()
-    for i in range(1):
+    
+    N = 4
+    fig, ax = plt.subplots(N)
+
+    for i in range(N):
+        ax[i].set_yscale("log", nonpositive='clip')
+        ax[i].set_ylim(bottom=1e-2, top=1e-1)
         y = obs.get_antenna(i)
-        acorr = np.correlate(y, y, mode = 'full')
-        plt.semilogy(acorr, label=f"{i}")
-        break
-    plt.legend()
+        centre = y.shape[0] // 2
+        z = acorr(y) / centre
+        noncentral_sample = z[centre-1000:centre-10]
+        sigma = np.mean(noncentral_sample)
+        ax[i].plot(z, label=f"Ant {i}: {sigma :6.3g}")
+        ax[i].legend()
+        ax[i].grid(True)
+    plt.savefig('autocorrelation.png')
     plt.show()
